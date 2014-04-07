2169964959ea2397df5bfc30d89b4615015149bf
Dan Liew <daniel.liew@imperial.ac.uk>
2014-04-07 18:42:47 +0100

    Passes implemented.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
-//#include "llvm/Bitcode/ReaderWriter.h"
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
+#include "llvm/PassManager.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/Transforms/IPO.h"
 #include <string>
+#include <vector>
 
 using namespace llvm;
 
@@ -32,7 +34,7 @@ int main(int argc, char** argv)
 
     Module* m = 0;
     SMDiagnostic Err; 
-    m = llvm::ParseIRFile(InputFilename, Err, llvm::getGlobalContext());
+    m = ParseIRFile(InputFilename, Err, llvm::getGlobalContext());
     if (!m)
     {
         errs() << "Failed to parsing " << InputFilename << "\n";
@@ -40,7 +42,27 @@ int main(int argc, char** argv)
         return 1;
     }
 
-
     m->dump();
+    PassManager* PM = new PassManager();
+
+    // inline functions with always_inline attribute
+    PM->add(createAlwaysInlinerPass());
+
+    std::vector<const char*> SymbolsToKeep;
+    SymbolsToKeep.push_back("main");
+
+    // Internalise everything except main
+    PM->add(createInternalizePass(SymbolsToKeep));
+
+    PM->add(createGlobalDCEPass());
+
+    PM->run(*m);
+    errs().changeColor(raw_ostream::Colors::RED, /*Bold=*/true, /*BG=*/false);
+    errs() << "\n\n\nresult:\n";
+    errs().resetColor();
+    m->dump();
+
+
+
     return 0;
 }
